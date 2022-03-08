061202b702ea4bf8d431d88a0a522564c2124a1d
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-08 17:56:59 +0530

    intoduction to programing

---
+++ Introduction-to-programing/intro.cpp
@@ -0,0 +1,20 @@
+#include <iostream> //including header file for input, output and cout, cin function
+using namespace std;
+
+int main()
+{ // starting main function
+
+    return 0; // return int value
+}
+
+// Programing Paradigm
+/*
+1.Iperative
+    1.1.Procedural programing laguage.
+    1.2.Object oriented progarming laguage.
+    1.3.Parallel Processing approach.
+2.Declarative
+    2.1.Logical programing language
+    2.2.Functional programing language
+    2.3.Database processing apporach
+*/
