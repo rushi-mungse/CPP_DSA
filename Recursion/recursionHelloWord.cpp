51434cbb21cfd5083f5637a098c72b4c2b47976a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-06-23 11:58:57 +0530

    Hello Word

---
+++ Recursion/recursionHelloWord.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printHelloWord()
+{
+    cout << "Hello Word" << endl;
+}
+
+int main()
+{
+    // print hello word 5 times is repeated
+    printHelloWord();
+    printHelloWord();
+    printHelloWord();
+    printHelloWord();
+    printHelloWord();
+    return 0;
+}
